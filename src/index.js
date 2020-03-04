7df03fd1cbfc7b799f3cce71beac7ca176fe0702
Garza0 <konovalovandrey7@gmail.com>
2020-03-05 00:52:03 +0200

    fix: misprint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   let pairs
-  for (let i = 0; i < bracketsConfig.length; i = 0) {
+  for (let i = 0; i < bracketsConfig.length;) {
     pairs = bracketsConfig[i].join('')
     if (str.includes(pairs)) {
       str = str.replace(pairs, '')
+      i = 0
     } else {
       i++
     }
